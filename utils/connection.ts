2d141af328c21cead1e8e22a0b975e944e1c3db5
Halvor Vivelstad <halvorviv@gmail.com>
2021-12-29 23:21:55 +0100

    Update connection.ts

--- utils/connection.ts
+++ utils/connection.ts
@@ -2,11 +2,11 @@
 import mongoose, { Model } from "mongoose";
 
 // CONNECTING TO MONGOOSE (Get Database Url from .env.local)
-const { MONGO_DATABASE_URL } = process.env;
+const { NEXT_PUBLIC_MONGO_DATABASE_URL } = process.env;
 
 // connection function
 export const connectDB = async () =>
   await mongoose
-    .connect(MONGO_DATABASE_URL as string)
+    .connect(NEXT_PUBLIC_MONGO_DATABASE_URL as string)
     .catch((err: any) => console.log(err));
 console.log("Mongoose Connection Established");
