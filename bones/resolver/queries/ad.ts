bbfc79a1623d48d83888800bea3ec1ab48480b11
Halvor Vivelstad <halvorviv@gmail.com>
2021-12-29 23:06:12 +0100

    :arrow_backward: Create queries for ads

--- bones/resolver/queries/ad.ts
+++ bones/resolver/queries/ad.ts
@@ -1,20 +1,18 @@
 import Ad from "../../models/ad";
 
-export const adQueries = {
-  getAds: async () => {
-    try {
-      const ads = await Ad.find({});
-      return ads;
-    } catch (error) {
-      console.error(error);
-    }
-  },
+export async function getAds() {
+  try {
+    const ads = await Ad.find({});
+    return ads;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-  getAd: async (_: any, { id }: any) => {
-    const ad = await Ad.findById(id);
-    if (!ad) {
-      throw new Error("Ad not found");
-    }
-    return ad;
-  },
-};
+export async function getAd(_: any, { id }: any) {
+  const ad = await Ad.findById(id);
+  if (!ad) {
+    throw new Error("Ad not found");
+  }
+  return ad;
+}
