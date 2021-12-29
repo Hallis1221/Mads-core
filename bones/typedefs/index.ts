6d1f895eae8d69fbb30a6f9e09d61a111195600a
Halvor Vivelstad <halvorviv@gmail.com>
2021-12-29 23:05:12 +0100

    :+1: Have typedefs be declared per use case

--- bones/typedefs/index.ts
+++ bones/typedefs/index.ts
@@ -1,55 +1,5 @@
 import { gql } from "apollo-server-micro";
+import { adTypeDefs } from "./ad";
+import { dataTypeDefs } from "./data";
 
-export const typeDefs = gql`
-  type Query {
-    getAds: [Ad]!
-    getAd(id: ID!): Ad
-  }
-
-  type Mutation {
-    createAd(input: AdInput): Ad
-    updateAd(id: ID!, input: AdInput): Ad
-    deleteAd(id: ID!): Ad
-  }
-
-  type Ad {
-    id: ID!
-    type: String!
-    theme: String!
-    title: String!
-    link: String!
-    image: String
-    tags: [Tag]!
-    owner: AdOwner!
-  }
-
-  input AdInput {
-    type: String!
-    theme: String!
-    title: String!
-    link: String!
-    image: String
-    owner: AdOwnerInput!
-    tags: [TagInput]!
-  }
-
-  type AdOwner {
-    uid: String!
-    displayName: String!
-  }
-
-  input AdOwnerInput {
-    uid: String!
-    displayName: String!
-  }
-
-  input TagInput {
-    tag: String!
-    priority: Int!
-  }
-
-  type Tag {
-    tag: String!
-    priority: Int!
-  }
-`;
+export const typeDefs = [adTypeDefs,dataTypeDefs];
