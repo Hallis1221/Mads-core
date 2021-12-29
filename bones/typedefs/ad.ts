f980c5e44d81aab3ae65ffc3325b68345cc7d347
Halvor Vivelstad <halvorviv@gmail.com>
2021-12-29 23:05:26 +0100

    :secret: Create seperate file for adtypedefs

---
+++ bones/typedefs/ad.ts
@@ -0,0 +1,55 @@
+import { gql } from "apollo-server-micro";
+
+export const adTypeDefs = gql`
+type Query {
+    getAds: [Ad]!
+    getAd(id: ID!): Ad
+  }
+
+  type Mutation {
+    createAd(input: AdInput): Ad
+    updateAd(id: ID!, input: AdInput): Ad
+    deleteAd(id: ID!): Ad
+  }
+
+  type Ad {
+    id: ID!
+    type: String!
+    theme: String!
+    title: String!
+    link: String!
+    image: String
+    tags: [Tag]!
+    owner: AdOwner!
+  }
+
+  input AdInput {
+    type: String!
+    theme: String!
+    title: String!
+    link: String!
+    image: String
+    owner: AdOwnerInput!
+    tags: [TagInput]!
+  }
+
+  type AdOwner {
+    uid: String!
+    displayName: String!
+  }
+
+  input AdOwnerInput {
+    uid: String!
+    displayName: String!
+  }
+
+  input TagInput {
+    tag: String!
+    priority: Int!
+  }
+
+  type Tag {
+    tag: String!
+    priority: Int!
+  }
+`;
