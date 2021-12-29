1c041c8b25a4e598831d288969dbef489829d7ae
Halvor Vivelstad <halvorviv@gmail.com>
2021-12-29 19:21:21 +0100

    :100: Create graphql endpoint

---
+++ pages/api/graphql.ts
@@ -0,0 +1,39 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { connect } from "../../utils/connection";
+import { ApolloServer,  } from "apollo-server-micro";
+import { typeDefs } from "../../bones/typeDefs";
+import resolvers from "../../bones/resolvers";
+
+connect();
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+const startServer = apolloServer.start();
+
+export default async function handler(req: any, res: any) {
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://studio.apollographql.com'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  if (req.method === 'OPTIONS') {
+    res.end()
+    return false
+  }
+
+  await startServer
+  await apolloServer.createHandler({
+    path: '/api/graphql',
+  })(req, res)
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
