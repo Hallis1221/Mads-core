733be195ee391cfddcedc424f014c22791090df8
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-01 22:42:43 +0100

    :bug: Fix

--- bones/auth/index.ts
+++ bones/auth/index.ts
@@ -1,2 +1,5 @@
-
 export const correctPassword = process.env.PASSWORD;
+
+export function authenticated(password: string): boolean {
+  return process.env.NODE_ENV !== "production" || password === correctPassword;
+}
