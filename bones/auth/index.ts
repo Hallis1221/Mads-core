8d5826a5b5aaed91cac6b5a646d7cfa2e74ac0f1
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-01 22:05:34 +0100

    :100: Let api work outside of localhost (fly little bird, fly!)

--- bones/auth/index.ts
+++ bones/auth/index.ts
@@ -1,10 +1,15 @@
 import { GraphQLClient } from "graphql-request";
 
 export const correctPassword = "h?LPhTi8ja8G!7Q@hGRsJx7444P6Sy?ob$ehS6mr";
-export var host: string = "http://localhost:3000";
+export var host: string | undefined;
 export var client = new GraphQLClient(`${host}/api/graphql`);
 
 
 export function authenticated(password: string): boolean {
   return ( process.env.NODE_ENV !== "production" && password === correctPassword) ;
+}
+
+export function setHost(newHost: string): void {
+  host = newHost;
+  client = new GraphQLClient(`${host}/api/graphql`);
 }
