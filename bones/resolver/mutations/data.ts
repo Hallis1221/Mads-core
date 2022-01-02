eae5d6804f6ae261355324be502af8a6db7f13d7
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-02 22:14:21 +0100

    wait a minute,thats my girl dog

--- bones/resolver/mutations/data.ts
+++ bones/resolver/mutations/data.ts
@@ -4,15 +4,15 @@ import registerContentClick from "../../data/registerContentClick";
 import registerContentView from "../../data/registerContentView";
 
 export async function registerViews(_: any, { adID, contentID }: any) {
-    registerAdView(adID);
-    registerContentView(contentID);
+    await registerAdView(adID);
+    await registerContentView(contentID);
 
     return true;
   }
 
   export async function registerClicks(_: any, { adID, contentID }: any) {
-    registerAdClick(adID);
-    registerContentClick(contentID);
+    await registerAdClick(adID);
+    await registerContentClick(contentID);
     return true;
   }
   
