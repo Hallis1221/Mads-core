62111b12e6c2ad2fdc9fee2a0968fa8150b38582
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-04 21:10:34 +0100

    :ab: Add more descriptive log before throwing error on getContentData

--- lib/resolver/queries/contentData.ts
+++ lib/resolver/queries/contentData.ts
@@ -3,6 +3,7 @@ import ContentData from "../../models/contentData";
 export async function getContentData(_: any, { contentID }: any) {
   const contentData = (await ContentData.findOne({ contentID }))
   if (!contentData) {
+    console.log("Content data not found for contentID: ", contentID);
     throw new Error("ContentData not found");
   }
   return contentData;
