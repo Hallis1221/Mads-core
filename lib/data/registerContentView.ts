bd399d8e6bee7815ae4dc5365127a54c847d08aa
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-04 21:09:59 +0100

    :seven: Simplify registerContentView and registerContentClick with the new requests system

--- lib/data/registerContentView.ts
+++ lib/data/registerContentView.ts
@@ -1,85 +1,16 @@
-import { gql } from "graphql-request";
 import { correctPassword } from "../auth";
-import { gqc} from "../network/client";
-
-const mutation = gql`
-  mutation Mutation($contentID: String!, $input: ContentDataInput) {
-    updateContentData(contentID: $contentID, input: $input) {
-      contentID
-      views
-    }
-  }
-`;
-
-const query = gql`
-  query Query($contentID: String!) {
-    getContentData(contentID: $contentID) {
-      views
-    }
-  }
-`;
-
-const createMutation = gql`
-  mutation Mutation($input: ContentDataCreateInput) {
-    createContentData(input: $input) {
-      contentID
-    }
-  }
-`;
+import { createContentData, getContentViews, updateContentViews } from "../requests/backend";
 
 // Export defualt function for registering a view. The function takes in ADid as a string as its only parameter.
 export default async function registerContentView(contentID: string): Promise<void> {
-
-  await gqc
-    .request(query, {
-      contentID: contentID,
-      password: correctPassword,
-    })
-    .then(async (contentData) => {
-      await incrementView(contentData);
-    })
-    .catch(async (_) => {
-      await createContentData();
-    });
-
-  function createContentData() {
-    gqc
-      .request(createMutation, {
-        input: {
-          contentID,
-          clicks: 0,
-          views: 0,
-          uploadDate: "null",
-          password: correctPassword,
-        },
-      })
-      .then((data) => {
-        console.log(
-          data.createContentData.title,
-          ":",
-          data.createContentData.id,
-          "created as its contentdata was not found"
-        );
-        registerContentView(data["createContentData"]["contentID"]);
-      });
-  }
-
-  async function incrementView(contentData: any) {
-    gqc
-      .request(mutation, {
-        contentID: contentID,
-        input: {
-          views: contentData.getContentData.views + 1,
-          password: correctPassword,
-        },
-      })
-      .then((data) => {
-        if (
-          data.updateContentData === null ||
-          data.updateContentData === undefined
-        ) {
-          createContentData();
-        }
-      });
-  }
+  const views = await getContentViews(contentID, correctPassword);
+  await updateContentViews(contentID, views + 1, correctPassword).catch(async (e) => {
+    console.log(
+      "Creating new content data. Likely beacuse the content data does not exist. ",
+      e
+    );
+    await createContentData(contentID, correctPassword);
+    registerContentView(contentID);
+    return;
+  });
 }
