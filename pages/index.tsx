b1a9292b24487f557620c029ec30c6159dc8b9aa
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-05 21:44:49 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,15 +7,16 @@ const Home: NextPage = () => {
 
       <div className="relative">
     
-          <div className="h-[100vh] bg-hero-image bg-cover"></div>
+          <div className="relative h-[210vh] w-[210vw] md:h-screen md:w-screen lg:h-screen lg:w-screen bg-hero-image bg-cover bg-no-repeat overflow-auto">
+          </div>
 
-        <div className="absolute w-full top-[40%]  text-center font-extrabold text-9xl mix-blend-overlay">
+        <div className="absolute w-screen top-[40%] left-[40%] md:left-0 lg:lef-0 xl:left-0  text-center font-extrabold text-9xl mix-blend-overlay">
   
           Coming 2022.
 
         </div>
         
-        <div className="absolute w-full top-[40%]  text-center font-extrabold text-9xl mix-blend-overlay">
+        <div className="absolute w-full top-[40%] left-[40%]  md:left-0 lg:lef-0 xl:left-0 text-center font-extrabold text-9xl mix-blend-overlay">
   
           Coming 2022.
 
