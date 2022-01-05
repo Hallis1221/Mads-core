663b89596d98af659f8b67bc665da02c923cf195
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-05 20:01:30 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import { Parallax, ParallaxProvider } from "react-scroll-parallax";
 
 const Home: NextPage = () => {
   return (
@@ -8,7 +7,7 @@ const Home: NextPage = () => {
 
       <div className="m-0 p-0 w-full h-full bg-top bg-white">
     
-          <div className="w-full h-screen absolute bg-hero-image bg-center"></div>
+          <div className="w-full h-screen absolute bg-hero-image bg-cover"></div>
 
         <div className="absolute w-full top-[40%]  text-center font-extrabold text-9xl mix-blend-overlay">
   
