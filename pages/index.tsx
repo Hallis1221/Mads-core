d41d4aa32b15f40afb6d594c64ac902a39cb1518
Hallis1221 <46447358+Hallis1221@users.noreply.github.com>
2022-01-06 12:11:41 +0100

    :x: Remove whitespaces

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,6 @@ const Home: NextPage = () => {
   return (
     <>
 
-
-
-
       <div className="w-screen h-screen relative justify-center">
         <Image
           layout="fill"
@@ -19,28 +16,26 @@ const Home: NextPage = () => {
           src={"https://pixelpoly.co/assets/img/portfolio/ctf.png"}
           alt={"x.bannerImage.alternativeText"}
         />
-
-
-
-<div className="z-10 absolute left-0 right-0 ml-auto mr-auto w-24 top-2/3 ">
-<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-32">
-  <Link href="/waitlist">
-    Join waitlist
-  </Link>
-</button>
-</div>
-        <div className="absolute text-center justify-center pb-24 flex flex-1 flex-col w-screen text-8xl lg:text-9xl xl:text-9xl h-screen font-extrabold mix-blend-overlay">
-
-          Coming 2022.
-
+        
+        <div className="z-10 absolute left-0 right-0 ml-auto mr-auto w-24 top-2/3 ">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-32">
+            <Link href="/waitlist">
+              Join waitlist
+            </Link>
+          </button>
         </div>
         <div className="absolute text-center justify-center pb-24 flex flex-1 flex-col w-screen text-8xl lg:text-9xl xl:text-9xl h-screen font-extrabold mix-blend-overlay">
 
           Coming 2022.
 
         </div>
+        <div className="absolute text-center justify-center pb-24 flex flex-1 flex-col w-screen text-8xl lg:text-9xl xl:text-9xl h-screen font-extrabold mix-blend-overlay">
 
-       
+          Coming 2022.
+
+        </div>
+
+
       </div>
 
     </>
