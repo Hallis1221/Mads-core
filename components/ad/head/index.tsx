e229208d30cc583fb84254b9b3c761ee18db24d8
Hallis1221 <46447358+Hallis1221@users.noreply.github.com>
2022-01-06 10:34:22 +0100

    :credit_card: Create header

--- components/ad/head/index.tsx
+++ components/ad/head/index.tsx
@@ -1,1 +1,17 @@
 // TODO make metatags and head component
+
+import Head from "next/head"
+import { ReactElement } from "react";
+
+export default function Header({ title = "", description = "", image = undefined }: any): ReactElement {
+    return (
+        <Head>
+            <meta property="og:title" content={title} />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://marketads.me" />
+            <meta property="og:image" content={image} />
+            <meta property="og:description" content={description} />
+            <meta name="theme-color" content="#FF0000">
+            <meta name="twitter:card" content="summary_large_image" />
+        </Head>);
+}
