46bffbb90bd8c2ba850d86cd2f6c5882f0cd45ac
Hallis1221 <46447358+Hallis1221@users.noreply.github.com>
2022-01-06 09:12:47 +0100

    :mag: Make ad image a bit smaller

--- components/ad/image/index.tsx
+++ components/ad/image/index.tsx
@@ -3,14 +3,14 @@ import { ReactElement } from "react";
 import { registerClick } from "../../../lib/logic/requests/frontend";
 
 export default function ImageAd({ad , content}: any): ReactElement {
-
-    return <div>
+  // perhaps the image should be bigger for big screens
+    return <div className="">
       <Image
         src={ad.image}
         alt="Deploy"
         priority={true}
-        height={630 * 1.2}
-        width={1200 * 1.2}
+        height={630 * 0.8}
+        width={1200 * 0.8}
         layout="intrinsic"
         className="h-full hover:cursor-pointer"
         onClick={async () => {
