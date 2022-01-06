f82f33de128ee8135c24ff3b68149279a1ebbfba
Hallis1221 <46447358+Hallis1221@users.noreply.github.com>
2022-01-06 12:28:10 +0100

    Create waitlist.tsx

---
+++ pages/waitlist.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next/types";
+import CornerLogo from "../components/logo";
+
+const Waitlist: NextPage = () => {
+    // https://getwaitlist.com
+    return (
+      <>
+  <CornerLogo/>
+  
+      </>
+    );
+  };
+  
+  export default Waitlist;
+  
