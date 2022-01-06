4983158b10bf558244e2f6a4dcd6779d9239191a
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-06 20:57:18 +0100

    Update waitlist.tsx

--- pages/waitlist.tsx
+++ pages/waitlist.tsx
@@ -1,15 +1,17 @@
+import Link from "next/link";
 import { NextPage } from "next/types";
 import CornerLogo from "../components/logo";
 
 const Waitlist: NextPage = () => {
-    // https://getwaitlist.com
-    return (
-      <>
-  <CornerLogo/>
-  
-      </>
-    );
-  };
-  
-  export default Waitlist;
-  
+  // https://getwaitlist.com
+  return (
+    <>
+      <CornerLogo />
+      <div className="w-screen h-screen flex flex-col justify-center items-center">
+        <Link href="/waitlist/creator">Creator Waitlist </Link>
+      </div>
+    </>
+  );
+};
+
+export default Waitlist;
