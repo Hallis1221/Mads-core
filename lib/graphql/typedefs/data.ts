8d58ecb535e45f7c5b826993b0a6dd94631fd4be
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-07 17:40:48 +0100

    :tada: Add registerSkips to the typedef

--- lib/graphql/typedefs/data.ts
+++ lib/graphql/typedefs/data.ts
@@ -5,5 +5,6 @@ export const dataTypeDefs = gql`
   type Mutation {
     registerViews(adID: ID!, contentID: ID!): Boolean
     registerClicks(adID: ID!, contentID: ID!): Boolean
+    registerSkips(adID: ID!, contentID: ID!): Boolean
   }
 `;
