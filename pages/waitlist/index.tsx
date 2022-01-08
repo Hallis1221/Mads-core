f4776330a336d35fa57716528463a41da2761435
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-08 14:33:02 +0100

    :truck: Move waitlist in pages directory

--- pages/waitlist/index.tsx
+++ pages/waitlist/index.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import { NextPage } from "next/types";
-import CornerLogo from "../components/logo";
+import CornerLogo from "../../components/logo";
+import NavBar from "../../components/navbar";
 
 const Waitlist: NextPage = () => {
   // https://getwaitlist.com
   return (
     <>
-      <CornerLogo />
-      <div className="w-screen h-screen flex flex-col justify-center items-center">
+   <NavBar />
+      <div className="w-screen h-full flex flex-col justify-center items-center">
         <Link href="/waitlist/creator">Creator Waitlist </Link>
       </div>
     </>
