8cffdda2e630dcd0d3256091eb0dbef1363d1449
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-08 13:55:12 +0100

    :articulated_lorry: Update CornerLogo

--- components/logo/index.tsx
+++ components/logo/index.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 export default function CornerLogo() {
   return (
     <Link href="/" passHref={true}>
-      <div className="items-end justify-start flex px-4 md:px-8 lg:px-16 xl:px-8 py-8 absolute z-20 hover:cursor-pointer">
+      <div className="px-4 md:px-8 lg:px-16 xl:px-8 py-8 z-20 bg-transparent w-64 hover:cursor-pointer">
         <Image
           src="/mads.svg"
           alt="Vercel Logo"
-          width={300 * 0.3}
-          height={128 * 0.3}
+          width={300 * 0.5}
+          height={128 * 0.5}
         />
       </div>
     </Link>
