14855b6a0aaec322bb3839c905481814d4d63962
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-08 18:14:02 +0100

    :credit_card: Create user model

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,10 +2,11 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../lib/utils/auth/authConnection";
+import User from "../../../lib/models/user";
 
 export default NextAuth({
   secret: process.env.NA_SECRET,
-  adapter: MongoDBAdapter(clientPromise),
+  adapter: MongoDBAdapter(clientPromise, ),
   providers: [
     GithubProvider({
       clientId: process.env.GH_ID,
@@ -14,15 +15,27 @@ export default NextAuth({
   ],
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
-        console.log(credentials);
+      console.log(user.email);
+      await User.findOneAndUpdate(
+        { email: user.email },
+        {
+          $set: {
+            creator: false,
+          },
+        },
+        { new: true }
+      )
+        .then(() => {
+          console.log("User updated");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
       return true;
     },
-    async session({ session, user, token }) {
-        console.log("user", user);
-      return session;
-    },
+
     async jwt({ token, user, account, profile, isNewUser }) {
-        console.log(isNewUser)
+      console.log(isNewUser);
       return token;
     },
   },
