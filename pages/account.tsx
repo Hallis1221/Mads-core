9c1317f260f3cc45780686740ef0d764ce884396
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-08 19:28:42 +0100

    :100: tell if user is creator on account page

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,15 +1,27 @@
 import { useSession, signIn, signOut, getProviders } from "next-auth/react";
+import { useEffect, useState } from "react";
 import SignInWithProvider from "../components/auth/signin";
 import NavBar from "../components/navbar";
+import {isCreator} from "../lib/logic/requests/frontend"
 
 export default function AccountPage({ providers }: any) {
   const { data: session } = useSession();
+  const [creator, setCreator] = useState(false);
+
+   useEffect(() => {
+    isCreator(  session?.user?.email || "" ).then( (res: any) => {
+      console.log(res)
+      setCreator(res)
+   })
+  }, [session])
+
   if (session) {
     return (
       <>
         <NavBar />
         <div className="w-screen h-full flex flex-col justify-center items-center">
           Signed in as {session.user?.email} <br />
+          {creator ? "Creator" : "Not Creator"}
           <button onClick={() => signOut()}>Sign out</button>
         </div>
       </>
