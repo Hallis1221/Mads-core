df8a90f632d55898711335db6ba039864405e5bf
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-08 13:50:38 +0100

    :wrench: Wrap app in sessionprovider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {Toaster} from 'react-hot-toast'
+import { SessionProvider, } from 'next-auth/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (<><Component {...pageProps} /><Toaster /></>)
+  return (<SessionProvider><Component {...pageProps} /><Toaster /></SessionProvider>)
 }
 
 export default MyApp
