e2c701f4953d222e61d573f824d7e8db6186aa5f
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-08 13:52:58 +0100

    :ab: Add navbar component

---
+++ components/navbar/index.tsx
@@ -0,0 +1,14 @@
+import TopAuth from "../auth/topbar";
+import CornerLogo from "../logo";
+
+export default function NavBar({ }) {
+  return (
+    // CSS row
+    <div className="flex flex-wrap-reverse justify-between bg-transparent">
+
+<CornerLogo />
+< TopAuth/>
+    </div>
+
+  );
+}
