f29b62c7ddbd52aa52586a4e6cdddefbd4046bf3
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-08 22:31:25 +0100

    :credit_card: Add credit page

---
+++ pages/credit.tsx
@@ -0,0 +1,9 @@
+export default function CreditPage({ }) {
+  return (
+    <main>
+        (Some) Designs <strike>stolen from</strike> by <a href="https://twitter.com/epicth3emilis">Epic</a>
+        <br />
+        Free QA by <a href="https://twitter.com/epicth3emilisn">Epic</a> 
+    </main>
+  );
+}
