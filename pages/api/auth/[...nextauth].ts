513f944cede705fc5251a7ce9fcb8f4a3b292f2a
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-08 17:29:15 +0100

    :tada: Add custom callbacks

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,29 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../lib/utils/auth/authConnection";
 
 export default NextAuth({
-    secret: process.env.NA_SECRET,
+  secret: process.env.NA_SECRET,
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GithubProvider({
       clientId: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+        console.log(credentials);
+      return true;
+    },
+    async session({ session, user, token }) {
+        console.log("user", user);
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+        console.log(isNewUser)
+      return token;
+    },
+  },
 });
