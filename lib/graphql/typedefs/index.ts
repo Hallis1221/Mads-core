53efb593367ec15d664ba227398308fc5c6458d1
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-08 19:30:44 +0100

    :tada: Add last to the typedefs list

--- lib/graphql/typedefs/index.ts
+++ lib/graphql/typedefs/index.ts
@@ -3,7 +3,8 @@ import { adDataTypeDefs } from "./adData";
 import { contentTypeDefs } from "./content";
 import { contentDataTypeDefs } from "./contentData";
 import { dataTypeDefs } from "./data";
+import { userTypeDefs } from "./user";
 import { waitlistTypeDefs } from "./waitlist";
 
 // Combine all the typeDefs into one array. 
-export const typeDefinitions = [adTypeDefs,adDataTypeDefs, contentTypeDefs, contentDataTypeDefs,dataTypeDefs, waitlistTypeDefs];
+export const typeDefinitions = [adTypeDefs,adDataTypeDefs, contentTypeDefs, contentDataTypeDefs,dataTypeDefs, waitlistTypeDefs, userTypeDefs];
