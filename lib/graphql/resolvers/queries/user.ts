5e784a2f3b5f1a62543eab36d391fbf5f670db7d
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-10 21:29:22 +0100

    :mag: Magic mail login

--- lib/graphql/resolvers/queries/user.ts
+++ lib/graphql/resolvers/queries/user.ts
@@ -1,18 +1,24 @@
 import User from "../../../models/user";
 
-  export async function checkAndDefaultRoles(_:any, {email}:any, { req}: any){
-      let user = await User.findOne({email: email});
-      if (user.creator !== false && user.creator != true) 
-       await User.findOneAndUpdate({email: email}, {creator: false}, {new: true});
-      console.log( await User.findOne({email: email}))
-  }
+export async function checkAndDefaultRoles(
+  _: any,
+  { email }: any,
+  { req }: any
+) {
+  let user = await User.findOne({ email: email });
+  if (!user?.creator || (user.creator !== false && user.creator != true))
+    await User.findOneAndUpdate(
+      { email: email },
+      { creator: false },
+      { new: true }
+    );
+  console.log(await User.findOne({ email: email }));
+  return isCreator(_, { email }, { req });
+}
 
-  export async function isCreator(_:any, {email}:any, { req}: any){
-      let user = await User.findOne({email: email});
-      try {
-      return user.creator;
-          
-      } catch (error) {
-          return false;
-      }
-  }
+export async function isCreator(_: any, { email }: any, { req }: any) {
+  let user = await User.findOne({ email: email });
+  if (user?.creator) return user.creator;
+
+  return false;
+}
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,17 +1,29 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import EmailProvider from "next-auth/providers/email";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../lib/utils/auth/authConnection";
-import User from "../../../lib/models/user";
 import { verifyRoles } from "../../../lib/logic/requests/frontend";
 
 export default NextAuth({
   secret: process.env.NA_SECRET,
-  adapter: MongoDBAdapter(clientPromise, ),
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GithubProvider({
       clientId: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
+    }),
+    EmailProvider({
+      server:
+        "smtp://" +
+        process.env.SMTP_USERNAME +
+        ":" +
+        process.env.SMTP_PASSWORD +
+        "@" +
+        process.env.SMTP_SERVER +
+        ":" +
+        process.env.SMTP_PORT,
+      from: process.env.EMAIL_FROM,
     }),
   ],
   callbacks: {
