0ec7c1843f0921681a8550c842ab5aa20e3ed782
Hallis1221 <46447358+Hallis1221@users.noreply.github.com>
2022-01-10 08:28:36 +0100

    :8ball: Make homepage work on school pc

--- pages/content/[id].tsx
+++ pages/content/[id].tsx
@@ -47,7 +47,7 @@ export default function AdPage(props: any): ReactElement {
       <Header title={content.title + " by " + content.owner.displayName + " | " + "Market Advertisments"} description={"Support " + content.owner.displayName + " by downloading " + content.title + " through mads!"} />
 
       <main className="min-h-screen  flex-1 flex flex-col">
-  < NavBar />
+        < NavBar />
         <div className="flex items-center flex-wrap flex-col relative pt-48 lg:relative lg:pt-32 md:pt-48">
           <div className="overflow-auto mb-4 p-0 pt-0 flex flex-col text-inherit border-2 border-solid border-gray-300 border-opacity-60 rounded-xl transition-colors duration-200 ease hover:text-blue-600 hover:border-blue-600 focus:text-blue-600 focus:border-blue-600 active:border-blue-600 active:text-blue-600">
             <MainAd ad={ad} content={content} setIsDone={setIsDone} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,17 +17,17 @@ export default function NewHome({}) {
             />
           
           </div>
-          <div className="pt-8 sm:pt-16 text-3xl sm:text-5xl lg:text-7xl xl:text-8xl 2xl:text-8xl font-extrabold">
+          <div className="pt-8 sm:pt-16 xl:pt-5 text-3xl sm:text-5xl lg:text-7xl xl:text-7xl 2xl:text-8xl font-extrabold">
             It´s time to ditch
             <br />
             AdFly and Linkvertise
           </div>
-          <div className="text-xl sm:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl pt-4 sm:pt-16 2xl:pt-24 w-11/12 lg:w-5/6 xl:w-4/6 2xl:w-6/12 font-semibold">
+          <div className="text-xl sm:text-2xl lg:text-3xl xl:text-2xl 2xl:text-5xl pt-4 sm:pt-16 xl:pt-16 2xl:pt-24 w-11/12 lg:w-5/6 xl:w-4/6 2xl:w-6/12 font-semibold">
             MarketAds is the brand new family-friendly way to help you monetize
             your Minecraft content. Coming 2022!
           </div>
           <div className="h-32">
-          <div className="text-lg sm:text-xl 2xl:text-xl flex flex-1 flex-col sm:flex-row items-start pt-5 sm:pt-20 justify-between sm:justify-start h-full   ">
+          <div className="text-lg sm:text-xl 2xl:text-xl flex flex-1 flex-col sm:flex-row items-start pt-5 sm:pt-20 xl:pt-10 justify-between sm:justify-start h-full   ">
             <button className="text-white px-4 sm:px-4 h-12 sm:h-20 w-46 sm:w-64 rounded-xl sm:rounded-3xl font-semibold bg-blue-600 hover:bg-blue-700">
               <Link href="/waitlist/creator">Join waitlist</Link>
             </button>
