a23c261f2da97ea17d362baf5a7cdbfe4bbfa88f
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-11 19:19:16 +0100

    :mag: Make sure user is signed in before fetching pfp

--- components/auth/topbar/index.tsx
+++ components/auth/topbar/index.tsx
@@ -5,14 +5,14 @@ import MD5 from "crypto-js/md5";
 
 export default function TopAuth() {
   const { data: session } = useSession();
-  if (session?.user)
+  if (session && session.user?.email)
     return (
       <div className="flex flex-1 flex-row sm:items-center justify-end pr-2 pt-2 w-auto">
         <div className="hidden sm:inline-block">{session.user.email} </div>
         <div className="sm:pl-5">
           <Link href="/account" passHref>
             <Image
-              src={session.user?.image || `https://www.gravatar.com/avatar/${MD5(session.user?.email).toString()}`}
+              src={session.user?.image || `https://www.gravatar.com/avatar/${MD5(session.user.email).toString()}`}
               alt="User profile picture"
               height={60}
               width={60}
