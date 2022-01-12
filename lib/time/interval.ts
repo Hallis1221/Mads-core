6233a1c2ea6a222e5c38a606a6e5dcd454d51fad
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-12 17:12:50 +0100

    :balloon: Allow createIntervaledTime to return a pair of begins and ends

--- lib/time/interval.ts
+++ lib/time/interval.ts
@@ -1,10 +1,19 @@
-export default function createIntervaledTime(): Date {
+export default function createIntervaledTime(
+  pair?: boolean
+): Date | { begins: Date; ends: Date } {
   let date = new Date();
   date.setMilliseconds(0);
   date.setSeconds(0);
 
   if (date.getMinutes() >= 30) date.setMinutes(30);
   else date.setMinutes(0);
+
+  // If pair is true, we want to return a pair of times, both the time decided by the function and the time that is 30 minutes after it.
+  if (pair)
+    return {
+      begins: new Date(date.getTime() + 30 * 60000),
+      ends: date,
+    };
 
   return date;
 }
