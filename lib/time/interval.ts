f45c8d3920a5ea99dcd87235701fe95ce87bb0f6
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-12 19:36:33 +0100

    :100: Seperate createIntervaledTimePair

--- lib/time/interval.ts
+++ lib/time/interval.ts
@@ -1,6 +1,4 @@
-export default function createIntervaledTime(
-  pair?: boolean
-): Date | { begins: Date; ends: Date } {
+export default function createIntervaledTime(): Date {
   let date = new Date();
   date.setMilliseconds(0);
   date.setSeconds(0);
@@ -8,12 +6,17 @@ export default function createIntervaledTime(
   if (date.getMinutes() >= 30) date.setMinutes(30);
   else date.setMinutes(0);
 
-  // If pair is true, we want to return a pair of times, both the time decided by the function and the time that is 30 minutes after it.
-  if (pair)
-    return {
-      begins: new Date(date.getTime() + 30 * 60000),
-      ends: date,
-    };
-
   return date;
+}
+
+export function createIntervaledTimePair(): {
+  begins: Date;
+  ends: Date;
+} {
+  let date = createIntervaledTime();
+  return {
+    begins: date,
+    ends: new Date(date.getTime() + 30 * 60000),
+ 
+  };
 }
