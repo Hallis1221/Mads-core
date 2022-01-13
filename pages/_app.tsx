8e16d640639b818e3090dd62db3fdb9127f30e30
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-13 18:03:47 +0100

    :a: Add session

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,8 @@ import type { AppProps } from 'next/app'
 import {Toaster} from 'react-hot-toast'
 import { SessionProvider, } from 'next-auth/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (<SessionProvider><Component {...pageProps} /><Toaster /></SessionProvider>)
+function MyApp({ Component, pageProps: {session,...pageProps} }: AppProps) {
+  return (<SessionProvider session={session}><Component {...pageProps} /><Toaster /></SessionProvider>)
 }
 
 export default MyApp
