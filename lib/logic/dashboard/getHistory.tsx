8bf4eb5133baafb3ea181b239735f8a2821d5fd9
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-21 21:15:23 +0100

    :imp: Improve accounteddailycontentperformance

--- lib/logic/dashboard/getHistory.tsx
+++ lib/logic/dashboard/getHistory.tsx
@@ -8,6 +8,7 @@ export default function getHistory(
 ) {
   if (!history) return;
 
+  let viewsSubtractions = 0;
   let dailyContentPerformances: Map<string, any> = new Map<string, any>();
 
   history.forEach(
@@ -75,8 +76,9 @@ export default function getHistory(
     if (value.skips < previousDayData.skips)
       value.skips += previousDayData.skips;
 
+    viewsSubtractions += previousDayData.views;
     accountedDailyContentPerformances.set(key, {
-      views: value.views - previousDayData.views,
+      views: value.views - viewsSubtractions,
       clicks: value.clicks - previousDayData.clicks,
       skips: value.skips - previousDayData.skips,
     });
@@ -99,5 +101,6 @@ export default function getHistory(
     }
   });
 
+  console.log(performance);
   return performance;
 }
