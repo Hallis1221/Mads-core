4c66e1b0c6b419cb198e9fbcd12a2e916e95e24d
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-22 14:52:06 +0100

    :white_check_mark: Create mongooose models

---
+++ lib/mongodb/models/adDataHistory.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+mongoose.Promise = global.Promise;
+
+const AdDataHistorySchema = new mongoose.Schema({
+  // The contentID of the content associated with the contentData is used as the primary key
+  adID: {
+    type: String,
+    required: true,
+  },
+
+  // The date for this history entry
+  date: {
+    type: Date,
+    required: true,
+  },
+
+  // The number of times the content has been clicked
+  clicks: {
+    type: Number,
+    required: false,
+  },
+
+  // The number of times this content has been viewed
+  views: {
+    type: Number,
+    required: false,
+  },
+
+  // The number of times this content has been skipped
+  skips: {
+    type: Number,
+    required: false,
+  },
+});
+
+const AdDataHistory =
+  mongoose.models.AdDataHistory ||
+  mongoose.model("AdDataHistory", AdDataHistorySchema);
+
+// EXPORTING OUR MODEL
+export default AdDataHistory;
