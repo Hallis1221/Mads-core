8365d8b513ccd0ab1805be9ef57c482517feeced
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-22 14:53:17 +0100

    :registered: Register views, clicks and skips using the new syste

--- lib/data/registerContentSkip.ts
+++ lib/data/registerContentSkip.ts
@@ -6,6 +6,7 @@ import {
   getContentSkips,
   updateContentSkips,
 } from "../logic/requests/backend";
+import ContentDataHistory from "../mongodb/models/contentDataHistory";
 
 // Export defualt function for registering a skip. The function takes in ADid as a string as its only parameter.
 export default async function registerContentSkip(
@@ -13,6 +14,36 @@ export default async function registerContentSkip(
 ): Promise<void> {
   // save amount of skips
   let skips = await getContentSkips(contentID, correctPassword);
+  let now = new Date(Date.now());
+  let date = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate()
+  );
+  date.setHours(0, 0, 0, 0);
+  // Try to update the skips.
+  let updated = await ContentDataHistory.updateOne(
+    {
+      contentID: contentID,
+      date: date,
+    },
+    {
+      $inc: {
+        skips: 1,
+      },
+    }
+  );
+
+  if (updated.modifiedCount === 0) 
+    // If the update failed, create a new history entry.
+    await ContentDataHistory.create({
+      contentID: contentID,
+      date: date,
+      skips: 1,
+      views: 0,
+      clicks: 0,
+    });
+
   // update skips by 1
   await updateContentSkips(contentID, skips + 1, correctPassword).catch(
     async (e) => {
