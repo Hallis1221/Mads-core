b6ee6692d9df4d67b4e1136f3fa29fb2c9fb867c
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-22 19:04:40 +0100

    :guitar: Add mock content elements

--- components/dashboard/chart/sidecard/index.tsx
+++ components/dashboard/chart/sidecard/index.tsx
@@ -2,8 +2,16 @@ import { ReactElement } from "react";
 
 export default function NewsCard(): ReactElement {
   return (
-    <div className="flex h-full w-full flex-1 flex-row justify-start pt-10">
-      <div className="grow h-96 w-10 bg-white rounded-3xl"></div>
+    <div className="flex h-full w-full flex-1 flex-row justify-start pt-10 font-poppins">
+      <div className="grow h-96 w-10 bg-white rounded-3xl text-center font-semibold text-2xl pt-3">
+        My content
+        <div className="flex flex-col justify-evenly h-full pb-16 pt-5">
+          <div className="font-normal py-1">Content 1</div>
+          <div className="font-normal py-1">Content 2</div>
+          <div className="font-normal py-1">Content 3</div>
+          <div className="font-normal py-1">Content 4</div>
+        </div>
+      </div>
     </div>
   );
 }
--- components/dashboard/sidebar/index.tsx
+++ components/dashboard/sidebar/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function SideBar() {
   return (
-    <div className="h-full w-80 bg-white rounded-r-3xl">
+    <div className="h-full w-80 bg-white rounded-r-3xl lg:hidden 2xl:block">
       <div className="flex flex-col pt-7">
         <Image src="/mads.svg" height={50} width={50} alt="Mads logo" />
       </div>
