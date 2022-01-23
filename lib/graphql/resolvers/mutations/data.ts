7f76420dd0695ab3ec88156fcdce610c82ae878a
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-23 02:16:56 +0100

    :tada: Add rate limiter to registerViews

--- lib/graphql/resolvers/mutations/data.ts
+++ lib/graphql/resolvers/mutations/data.ts
@@ -4,14 +4,33 @@ import registerAdView from "../../../data/registerAdView";
 import registerContentClick from "../../../data/registerContentClick";
 import registerContentSkip from "../../../data/registerContentSkip";
 import registerContentView from "../../../data/registerContentView";
+import {getGraphQLRateLimiter} from "graphql-rate-limit";
 
 // This is the resolver for the registerViews mutation. It takes in both an adID and contentID and registers the adView and contentView for the adID and contentID.
 // This is done to have a single resolver for updating both the adView and contentView.
-export async function registerViews(_: any, { adID, contentID }: any) {
+export async function registerViews(
+  parent: any,
+  args: any,
+  context: any,
+  info: any
+) {
+  const rateLimiter = getGraphQLRateLimiter({
+    identifyContext: (ctx) => {
+      let headers = ctx.req.socket._httpMessage.req.rawHeaders;
+      console.log(headers[headers.length - 1]);
+      return headers[headers.length - 1];
+    },
+  });
+
+  const errorMessage = await rateLimiter(
+    { parent, args, context, info },
+    { max: 5, window: '10s' }
+  );
+  if (errorMessage) throw new Error(errorMessage);
   // Register the adView for the adID.
-  await registerAdView(adID);
+  await registerAdView(args.adID);
   // Register the contentView for the contentID.
-  await registerContentView(contentID);
+  await registerContentView(args.contentID, parent, args, context, info);
   // Return true to indicate that the views were registered.
   return true;
 }
