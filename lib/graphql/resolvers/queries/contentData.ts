9d03a3f337edde1b63a285f7dfbf8a4538d8f6ca
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-23 01:10:00 +0100

    :sa: Start authentication on getContentData

--- lib/graphql/resolvers/queries/contentData.ts
+++ lib/graphql/resolvers/queries/contentData.ts
@@ -1,7 +1,17 @@
+import { authenticated } from "../../../auth";
+import getOwner from "../../../data/owns";
 import ContentData from "../../../mongodb/models/contentData";
 
 // This is the resolver for the getContentData query. It takes in the contentID and returns the contentData with the matching contentID.
-export async function getContentData(_: any, { contentID }: any) {
+export async function getContentData(_: any, { contentID, password }: any, { user }: any) {
+  // Check that the user is authenticated. 
+  // TODO
+  if (
+    ((password && authenticated(password) && contentID) ||
+      (user && user.uid && (await getOwner(contentID)).uid === user.uid)) ===
+    false
+  )
+    throw new Error("Unauthorized");
   try {
     // Find the contentData with the matching contentID.
     const contentData = await ContentData.findOne({ contentID });
