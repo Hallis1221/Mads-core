f467135f1f05e5ea3f252603e2999353c69333a1
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-25 17:21:56 +0100

    :+1: Factoring

--- lib/data/owns.ts
+++ lib/data/owns.ts
@@ -1,17 +1,13 @@
 import Content from "../mongodb/models/content";
 
 // function that checks the owner of provided contentID
-export default async function getOwner(
-    contentID: string,
-)
-{
-    let content = await Content.findOne({
-        where: {
-            id: contentID,
-        },
-    });
-    if (!content) {
-        throw new Error(`Content with id ${contentID} does not exist`);
-    }
-    return content.owner;
+export default async function getOwner(contentID: string) {
+  let content = await Content.findOne({
+    _id: contentID,
+  });
+  if (!content) {
+    throw new Error(`Content with id ${contentID} does not exist`);
+  }
+
+  return content.owner;
 }
