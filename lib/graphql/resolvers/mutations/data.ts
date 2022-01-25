536f6dafd3cec348708d64dea2d7e9d46ebdecc0
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-25 19:53:18 +0100

    :chart: Change rate limiter window

--- lib/graphql/resolvers/mutations/data.ts
+++ lib/graphql/resolvers/mutations/data.ts
@@ -32,7 +32,7 @@ export async function registerViews(
 
   let error = await rateLimiter(
     { parent, args, context, info },
-    { max: 3, window: "60s" }
+    { max: 3, window: "10000" }
   );
 
   if (error) throw new Error(error);
