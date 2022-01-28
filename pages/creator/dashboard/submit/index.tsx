2f7523265604764b099a088ca5cbb5220e630ead
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-28 16:05:39 +0100

    :+1: Ensure authentication on SubmissionPage

--- pages/creator/dashboard/submit/index.tsx
+++ pages/creator/dashboard/submit/index.tsx
@@ -1,6 +1,13 @@
+import { useSession } from "next-auth/react";
 import SideBar from "../../../../components/dashboard/sidebar";
+import { createUserContent } from "../../../../lib/logic/requests/frontend";
 
 export default function SubmissionPage({}) {
+  // Get authentication session
+  const {data: session}= useSession();
+
+  if (!session?.user) return <div>Not signed in</div>;
+ //  console.log(createUserContent("test", "test", ["tags"]));
   return (
     <main>
       <div className="relative h-screen w-full bg-[#F2F7FF] flex flex-row font-mulish">
