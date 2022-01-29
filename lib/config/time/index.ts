54fbd6c56bb56a8c7f5ff2ae119ea40f467d2c72
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-29 18:48:48 +0100

    :a: Add config for interval times

---
+++ lib/config/time/index.ts
@@ -0,0 +1,9 @@
+export const intervalTimes: {
+  minutes: number | undefined;
+  hours: number | undefined;
+  days: number | undefined;
+} = {
+  minutes: 30,
+  hours: undefined,
+  days: undefined,
+};
