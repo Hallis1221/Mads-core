6fdda94ef0cc18f94c37cc6c126436b4a66bcc90
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-30 14:48:42 +0100

    :1234: Improve isAuthorized for user authentication

--- lib/auth/checks/index.ts
+++ lib/auth/checks/index.ts
@@ -9,7 +9,6 @@ export async function isAuthorized(
   authentication: User | string | undefined,
   context: {
     contentid: string | undefined;
-    user: User | undefined;
   }
 ): Promise<boolean> {
   // If the access level is not defined, throw an error
@@ -19,6 +18,7 @@ export async function isAuthorized(
   if (!authentication) throw new Error("Authentication is undefined");
 
   // If the accesslevel is none, return true
+  // TODO implement rate limiting
   if (accessLevel === "none") return true;
 
   // If a api key is provided as the authentication and the key is valid, for now give the user access to everything.
@@ -31,16 +31,20 @@ export async function isAuthorized(
   // Then check if an email is provided in the user, if not, throw an error
   // Then check if the user is an admin
 
+if (typeof authentication !== "object")
+    throw new Error("Authentication is not an object");
+
+
   if (accessLevel === "admin") {
-    if (!context.user) throw new Error("User is undefined");
-    else if (!context.user.email) throw new Error("User email is undefined");
-    else if (await isAdmin(context.user.email)) return true;
+    if (!authentication) throw new Error("User is undefined");
+    else if (!authentication.email) throw new Error("User email is undefined");
+    else if (await isAdmin(authentication.email)) return true;
   }
 
   // If the access level is user, check if a user is provided in the context, if not, throw an error
   if (accessLevel === "user") {
-    if (!context.user) throw new Error("User is undefined");
-    else if (context.user.email && context.user.email) return true;
+    if (!authentication) throw new Error("User is undefined");
+    else if (authentication.email && authentication.email) return true;
     else throw new Error("Some of the properties on user is undefined");
   }
 
@@ -49,9 +53,9 @@ export async function isAuthorized(
   // Then check if the user is a creator
 
   if (accessLevel === "creator") {
-    if (!context.user) throw new Error("User is undefined");
-    if (!context.user.email) throw new Error("User email is undefined");
-    else if (await isCreator(context.user.email)) return true;
+    if (!authentication) throw new Error("User is undefined");
+    if (!authentication.email) throw new Error("User email is undefined");
+    else if (await isCreator(authentication.email)) return true;
     else throw new Error("User is not a creator");
   }
 
@@ -61,10 +65,10 @@ export async function isAuthorized(
   // Then check if the user is the owner of the content
 
   if (accessLevel === "owner") {
-    if (!context.user) throw new Error("User is undefined");
-    else if (!context.user.email) throw new Error("User email is undefined");
+    if (!authentication) throw new Error("User is undefined");
+    else if (!authentication.email) throw new Error("User email is undefined");
     else if (!context.contentid) throw new Error("Content id is undefined");
-    else if (await isContentOwner(context.user, context.contentid)) return true;
+    else if (await isContentOwner(authentication, context.contentid)) return true;
     else throw new Error("User is not the owner of the content");
   }
 }
