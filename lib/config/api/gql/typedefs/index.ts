a72e30a73f27c52b4aa178d36d2d586a3d4952b4
Halvor Vivelstad <halvorviv@gmail.com>
2022-01-30 17:07:43 +0100

    :cd: Create type defs

---
+++ lib/config/api/gql/typedefs/index.ts
@@ -0,0 +1,6 @@
+import { DocumentNode } from "graphql";
+import { waitlistTypeDefs } from "./waitlist";
+
+const typeDefinitions: Array<DocumentNode> = [waitlistTypeDefs, ]
+
+export default typeDefinitions
