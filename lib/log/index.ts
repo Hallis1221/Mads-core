4fb0cc94d89f13217972519544f0b72089943809
Halvor Vivelstad <halvorviv@gmail.com>
2022-02-06 18:28:59 +0100

    :a: Add logger for production

--- lib/log/index.ts
+++ lib/log/index.ts
@@ -1,16 +1,12 @@
 import winston from "winston";
 
 export const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.colorize(),
-  });
-  
-  //
-  // If we're not in production then log to the `console` with the format:
-  // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-  //
-  if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-      format: winston.format.simple(),
-    }));
-  }
+  level: "info",
+  format: winston.format.colorize(),
+});
+
+logger.add(
+  new winston.transports.Console({
+    format: winston.format.simple(),
+  })
+);
