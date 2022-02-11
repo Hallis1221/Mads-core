5815bb50926e7a82d587ce51b787e2919ec01e7a
Halvor Vivelstad <halvorviv@gmail.com>
2022-02-11 21:12:25 +0100

    :abc: Add a few comments

--- lib/server/user/defaultUser.ts
+++ lib/server/user/defaultUser.ts
@@ -2,15 +2,14 @@ import UserDB from "../../db/models/auth/user";
 import { logger } from "../../log";
 import { User } from "../../types/user";
 
-export default async function defaultUser (userID: string): Promise<User> {
-    let user = await UserDB.findOne({ userID });
+export default async function defaultUser(userID: string): Promise<User> {
+  let user = await UserDB.findOne({ userID });
 
-if (!user) throw new Error(`User with id: ${userID} not found`);
+  if (!user) throw new Error(`User with id: ${userID} not found`);
 
-if (!user.creator) {
-  await UserDB.updateOne({ email: user.email }, { creator: false });
+  if (!user.creator)
+    await UserDB.updateOne({ email: user.email }, { creator: false });
 
-  logger.debug(`Defaulted user: ${user.email} to creator: false`);
-}
-return user;
+  logger.debug(`Defaulted user: ${user.email} to creator: ${user.creator}`);
+  return user;
 }
