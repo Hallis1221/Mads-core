8b1c73d18c0ffaaf582fb3cd601612e86c7d6a74
Halvor Vivelstad <halvorviv@gmail.com>
2022-02-12 16:45:46 +0100

    :capital_abcd: Add getUserStripeIDQuery

--- lib/api/gql/resolvers/queries/user/index.ts
+++ lib/api/gql/resolvers/queries/user/index.ts
@@ -1,6 +1,8 @@
 import { isAuthorized } from "../../../../../auth/checks";
 import { isCreator } from "../../../../../auth/checks/user";
+import ApiDB from "../../../../../db/models/auth/api";
 import UserDB from "../../../../../db/models/auth/user";
+import { getUserStripeID } from "../../../../../server/user";
 import { User } from "../../../../../types/user";
 
 // This is the resolver for the isCreator query. It takes in the userID and adID as its only argument and returns true if the user is a creator
@@ -13,4 +15,29 @@ export async function isCreatorQuery(
   else if (user) return await isCreator(user.email);
 
   return false;
+}
+
+// This is the resolver for the getUserStripeID query.
+export async function getUserStripeIDQuery(
+  _: undefined,
+  { apiKey }: { apiKey: string },
+  { user }: { user: User }
+): Promise<string> {
+  if (apiKey) {
+    if (!(await isAuthorized("creator", apiKey, { contentid: undefined })))
+      throw new Error("API key is not authorized as creator");
+
+    // Get the user id from the api key
+    let userID: string | undefined = (await ApiDB.findOne({ apiKey })).userID;
+
+    if (!userID) throw new Error("API key is not associated with a user");
+
+    return await getUserStripeID(userID);
+  }
+  if (user) {
+    const userDB = await UserDB.findOne({ email: user.email || user.user.email });
+    return getUserStripeID(userDB._id);
+  }
+
+  throw new Error("Unathorized");
 }
