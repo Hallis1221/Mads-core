c508478fee2a45164da6b17934c45aa91802481a
Halvor Vivelstad <halvorviv@gmail.com>
2022-02-12 16:41:50 +0100

    :ram: Rename defualtUser.ts to user/index.ts

---
+++ lib/server/user/index.ts
@@ -0,0 +1,44 @@
+import UserDB from "../../db/models/auth/user";
+import { logger } from "../../log";
+import { User } from "../../types/user";
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export default async function defaultUser(userID: string): Promise<User> {
+  let user = await UserDB.findOne({ userID });
+
+  if (!user) throw new Error(`User with id: ${userID} not found`);
+
+  if (!user.creator)
+    await UserDB.updateOne({ email: user.email }, { creator: false });
+
+  logger.debug(`Defaulted user: ${user.email} to creator: ${user.creator}`);
+  return user;
+}
+
+export async function getUserStripeID(userID: string): Promise<string> {
+let user = await UserDB.findById(userID);
+
+  if (!user._id) throw new Error(`User with id: ${userID} not found`);
+  if (!user.stripeID)
+    throw new Error(`User with id: ${userID} has no stripeID`);
+  return user.stripeID;
+}
+
+export async function createUserStripeID(userID: string): Promise<string> {
+  let user = await UserDB.findOne({ userID });
+
+  if (!user._id) throw new Error(`User with id: ${userID} not found`);
+  if (user.stripeID)
+    throw new Error(`User with id: ${userID} already has a stripeID. Call getUserStripeID to get the stripeID`);
+
+  const account = await stripe.accounts.create({
+    type: "express",
+  });
+
+  await UserDB.findByIdAndUpdate(userID, { stripeID: account.id });
+
+  logger.debug(`Created user: ${user.email} stripeID: ${account.id}`);
+
+  return await getUserStripeID(userID);
+}
