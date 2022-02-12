ec62aee399743eebf8510cf3d56cd4e082f5ce3b
Halvor Vivelstad <halvorviv@gmail.com>
2022-02-12 16:44:46 +0100

    :ab: Add createUserStripeID and getUserStripeID functions

--- lib/api/gql/resolvers/mutations/user/index.ts
+++ lib/api/gql/resolvers/mutations/user/index.ts
@@ -1,9 +1,12 @@
+import { isAuthorized } from "../../../../../auth/checks";
+import ApiDB from "../../../../../db/models/auth/api";
 import UserDB from "../../../../../db/models/auth/user";
 import { logger } from "../../../../../log";
+import { createUserStripeID } from "../../../../../server/user";
 import { User } from "../../../../../types/user";
 
 // This is the resolver for the defualtUser mutation.
-export default async function defaultUserMutation(
+export async function defaultUserMutation(
   _: undefined,
   { userID }: { userID: string }
 ): Promise<User> {
@@ -18,4 +21,30 @@ export default async function defaultUserMutation(
   }
 
   return user;
+
+}
+
+// This is the resolver for the createUserStripeID mutation.
+export async function createUserStripeIDMutation(
+  _: undefined,
+  { apiKey }: { apiKey: string },
+  { user }: { user: User },
+): Promise<string> {
+  if (apiKey) {
+    if (!(await isAuthorized("creator", apiKey, { contentid: undefined })))
+      throw new Error("API key is not authorized as creator");
+
+    // Get the user id from the api key
+    let userID: string | undefined = (await ApiDB.findOne({ apiKey })).userID;
+
+    if (!userID) throw new Error("API key is not associated with a user");
+
+    return await createUserStripeID(userID);
+  }
+  if (user) {
+    const userDB = await UserDB.findOne({ email: user.email });
+    return createUserStripeID(userDB._id);
+  }
+
+  throw new Error("Unathorized");
 }
