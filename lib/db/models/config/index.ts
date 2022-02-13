c55789361501bee2864484c27fbeeaf4ca126b64
Halvor Vivelstad <halvorviv@gmail.com>
2022-02-13 14:03:45 +0100

    :pear: Add configuration schema object

---
+++ lib/db/models/config/index.ts
@@ -0,0 +1,51 @@
+// This is the model for the configuration object. 
+
+import mongoose from "mongoose";
+
+mongoose.Promise = global.Promise;
+
+const ConfigSchema = new mongoose.Schema({
+    // The name of the configuration object
+    name: {
+        type: String,
+        required: true,
+    },
+
+    // The prices for advertisments
+    prices: {
+        type: {
+            // The price for a view
+            view: {
+                type: Number,
+                required: true,
+            },
+
+            // The price for a click
+            click: {
+                type: Number,
+                required: true,
+            },
+        }
+    },
+
+    // Platform fees
+    platformFees: {
+        type: {
+            // The platform fee for advertisers
+            advertiser: {
+                type: Number,
+                required: true,
+            },
+
+            // The platform fee for creators
+            creator: {
+                type: Number,
+                required: true,
+            },
+        }
+    },
+
+});
+
+const ConfigDB = mongoose.models.Config || mongoose.model("Config", ConfigSchema);  
+export default ConfigDB;
