d558f5bfd42dd937fd836e904efd965cb56fedca
Halvor Vivelstad <halvorviv@gmail.com>
2022-02-15 18:34:28 +0100

    :dvd: Support both pending and complete in calculateAccountPaidout

--- lib/server/currency/calculateEarnings.ts
+++ lib/server/currency/calculateEarnings.ts
@@ -6,68 +6,65 @@ import { Content } from "../../types/content";
 import { ContentData } from "../../types/data/contentData";
 import { getUserContentData } from "../content/data/getContentData";
 
-export default async function calculateAccountEarnings(
-    userID: string,
-){
-    let prices = (await ConfigDB.findOne({
-        name: "prototyping"
-    }).select("prices"))?.prices;
+export default async function calculateAccountEarnings(userID: string) {
+  let prices = (
+    await ConfigDB.findOne({
+      name: "prototyping",
+    }).select("prices")
+  )?.prices;
 
-    if (!prices) throw new Error("No prices found");
-    
-    // Get the user's content data
-    const userContent: any= await getUserContentData(userID);
+  if (!prices) throw new Error("No prices found");
 
-    // Store totals for views and clicks
-    let totalViews = 0;
-    let totalClicks = 0;
-    
-    // Iterate through the user's content
-    for (var content of userContent) {
+  // Get the user's content data
+  const userContent: any = await getUserContentData(userID);
 
-        content = content[0] as ContentData;
+  // Store totals for views and clicks
+  let totalViews = 0;
+  let totalClicks = 0;
 
-        // Add the content's views to the total
-        totalViews += content.views || 0;
+  // Iterate through the user's content
+  for (var content of userContent) {
+    content = content[0] as ContentData;
 
-        // Add the content's clicks to the total
-        totalClicks += content.clicks || 0;
-    }
-        
-    // Calculate the earnings
-    const earnings = (totalClicks  * prices.click) + (totalViews  * prices.view);
+    // Add the content's views to the total
+    totalViews += content.views || 0;
 
-    return earnings;
+    // Add the content's clicks to the total
+    totalClicks += content.clicks || 0;
+  }
+
+  // Calculate the earnings
+  const earnings = totalClicks * prices.click + totalViews * prices.view;
+
+  return earnings;
 }
 
-export async function calculateAccountPaidout(
-    userID: string,
-    type: string,
-){
-    if (type === "any"){
-        let paidout = 0;
-        const payments = await PaymentsDB.find({
-        status: "complete" || "pending",
-            userID
-        });
-        for (var payment of payments){
-            paidout += payment.amount;
-        }
-        return paidout;
-    }
+export async function calculateAccountPaidout(userID: string, type: string) {
+  if (type === "any") {
+    let paidout = 0;
 
-    let payments = (await PaymentsDB.find({
-        userID: userID,
-        status: "complete" || "pending",
-        type: type,
-    }).select("amount"));
+    // Find all payments in the PaymentsDB that have a status of pending or complete
+    const payments = await PaymentsDB.find({
+      userID,
+      $or: [{ status: "pending" }, { status: "complete" }],
+    });
 
-    let total = 0;
+    for (var payment of payments) paidout += payment.amount;
 
-    for (var payment of payments) {
-        total += payment.amount;
-    }
-    
-    return total;
+    return paidout;
+  }
 
+  let payments = await PaymentsDB.find({
+    userID: userID,
+    $or: [{ status: "pending" }, { status: "complete" }],
+    type: type,
+  }).select("amount");
+
+  let total = 0;
+
+  for (var payment of payments) {
+    total += payment.amount;
+  }
+
+  return total;
 }
