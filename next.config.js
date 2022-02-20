c8e5e66c98e86fd7204b1033b3a2e57b4a66944c
Halvor Vivelstad <halvorviv@gmail.com>
2022-02-20 13:37:13 +0100

    :a: Add marketads as valid domain

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['pixelpoly.co', 'www.5framestudios.com', "avatars.githubusercontent.com", "www.gravatar.com"],
+    domains: ['pixelpoly.co', 'www.5framestudios.com', "avatars.githubusercontent.com", "www.gravatar.com", "www.marketads.me"],
   },
 }
